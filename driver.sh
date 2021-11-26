1b344089ae78a0df860af317a904d8ae8770361d
rongli6 <lirnishui@gmail.com>
2021-11-25 21:38:24 -0800

    Adding eda.Rmd file as a child file

--- driver.sh
+++ driver.sh
@@ -5,7 +5,7 @@ python src/DownloadData.py --URL=https://raw.githubusercontent.com/rfordatascien
 python src/DataProcessing.py --source_data=data/raw_data.csv --output_dir=data/processed
 
 # Exploratory Data Analysis
-python src/generate_eda.py --filepath=data/processed/train_df.csv --outfigure=results/figures/eda.png
+python src/generate_eda.py --filepath=data/processed/train_df.csv --outfigure=results/figures/eda_target_distribution.png --outfigure2=results/figures/eda_category_distribution.png --outcsv=results/tables/eda_summary_table.csv
 
 # Model Tuning and Fitting 
 
--- src/generate_eda.py
+++ src/generate_eda.py
@@ -1,12 +1,14 @@
 # author: Rong Li
 # date: 2021-11-25
 
-"""This script using the training dataset and produce figure 
-Usage: generate_eda.py --filepath=<filepath> --outfigure=<outpath>
+"""This script using the training dataset and produce figures 
+Usage: generate_eda.py --filepath=<filepath> --outfigure=<outfigure> --outfigure2=<outfigure2> --outcsv=<outcsv>
 
 Options:
 --filepath=<filepath>     Local filepath for training dataset with filename (this is a required option)
---outfigure=<outfigure>   Local filepath for figures with filename (this is a required option)
+--outfigure=<outfigure>   Local filepath for saving figure with filename (this is a required option)
+--outfigure2=<outfigure2> Local filepath for saving figure 2 with filename (this is a required option)
+--outcsv=<outcsv>         Local filepath for saving csv files with filename (this is a required option)
 """ 
 
 from docopt import docopt
@@ -16,17 +18,61 @@ alt.data_transformers.disable_max_rows()
 opt = docopt(__doc__)
 
 def main(opt):
-    
+    # Interpret dictionary
     filepath = opt["--filepath"]
     outfigure = opt["--outfigure"]
+    outfigure2 = opt["--outfigure2"]
+    outcsv = opt["--outcsv"]
     
+    # Read training dataset
     train_df = pd.read_csv(filepath)
-    
-    chart = alt.Chart(train_df).mark_bar().encode(
-        alt.X("annual_salary", bin=alt.Bin(maxbins=200), title = "Annual Salary"),
-        y='count()')
 
+    # Save a figure for the target distribution
+    chart = alt.Chart(train_df).mark_bar(clip=True).encode(
+            alt.X("annual_salary", bin=alt.Bin(maxbins=200), scale=alt.Scale(domain=(0, 500000)), title = "Annual Salary"),
+            y='count()')
+    chart = chart + alt.Chart(train_df).mark_rule(color="red").encode(
+        x=alt.X("median(annual_salary)"),
+    )
     chart.save(outfigure)
+
+    # Define a function that finds unique values for dataframe columns
+    def df_uniques(df):
+        result = {}
+        for x in df.columns:
+            result[df[x].name] = df[x].unique().tolist()[0:5]
+        return result
+    
+    # Save a summary table for cateforical features as csv file
+    train_X = train_df.drop(columns=["annual_salary"])
+    result = {}
+    result["Not Null Count"] = train_X.notnull().sum()
+    result["Null Count"] = train_X.isnull().sum()
+    result["Number of Unique Values"] = train_X.nunique()
+    result["Some Unique Values"] = df_uniques(train_X)
+    result["Types"] = train_X.dtypes
+    result_df = pd.DataFrame(result)
+    result_df.index.names = ['Features'] 
+    result_df.to_csv(outcsv)
+    
+
+    # Save a figure for the categorical features
+    col = result_df[result_df["Number of Unique Values"] < 20]
+    collist = col.index.tolist()
+    
+    chart =alt.Chart(train_df).mark_bar(clip=True).encode(
+            x=alt.X("median(annual_salary)",scale=alt.Scale(domain=(0, 200000)), title = "Median Annual Salary"),
+            y=alt.Y(alt.repeat(), type='ordinal',sort='x'),
+        ).properties(
+            width=300,
+            height=200
+        ).repeat(
+            collist,
+            columns=2
+        ) 
+
+    chart.save(outfigure2)
+    
 
 if __name__ == "__main__":
     main(opt)
