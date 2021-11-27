c52d965e90ee6b7e97d504495b41fa14082a0627
Cuthbert Chow <cuthbert.chow@gmail.com>
2021-11-27 12:32:50 -0800

    Update README With Usage

--- driver.sh
+++ driver.sh
@@ -8,23 +8,7 @@ python src/DataProcessing.py --source_data=data/raw_data.csv --output_dir=data/p
 python src/generate_eda.py --filepath=data/processed/train_df.csv --outfigure=results/figures/eda_target_distribution.png --outfigure2=results/figures/eda_category_distribution.png --outcsv=results/tables/eda_summary_table.csv
 
 # Model Tuning and Fitting 
-
+python src/fit_transform_evaluate_model.py --source_data=data/processed --output_dir=results
 
 # Creating Final Report
 Rscript -e "rmarkdown::render('doc/final_report.Rmd', output_format = 'html_document')"
-
-
-
-
-
-# # create exploratory data analysis figure and write to file 
-# Rscript src/eda_wisc.r --train=data/processed/training.feather --out_dir=results
-
-# # tune model
-# Rscript src/fit_breast_cancer_predict_model.r --train=data/processed/training.feather --out_dir=results
-
-# # test model
-# Rscript src/breast_cancer_test_results.r --test=data/processed/test.feather --out_dir=results
-
-# # render final report
-# Rscript -e "rmarkdown::render('doc/breast_cancer_predict_report.Rmd', output_format = 'github_document')"
--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -9,6 +9,7 @@ Options:
 --output_dir=<filepath>      Local filepath with filename (this is a required option)
 """ 
 
+#Import Dependencies
 from docopt import docopt
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,14 +17,17 @@ from sklearn.model_selection import train_test_split
 opt = docopt(__doc__)
 
 def main(opt):
+    # Read raw CSV files
     source = opt['--source_data']
     filepath = opt['--output_dir']
     df = pd.read_csv(source)
 
+    # Remove Currency Column from Data
     df_dropcurrency = df[df["currency"] == "USD"].drop(
         columns=["currency", "currency_other"]
     )
     
+    # Keep only country values equivalent to 'US', and drop unused columns
     df_clean = df_dropcurrency[
         df_dropcurrency["country"]
         .str.lower()
@@ -40,6 +44,7 @@ def main(opt):
         )
     ].drop(columns=['country', 'race', 'gender', 'timestamp',"additional_context_on_job_title", "additional_context_on_income"])
 
+    # Create train and test datasets, export as CSV
     train_df, test_df = train_test_split(df_clean, test_size=0.3, random_state=123)
     train_df.to_csv(filepath + '/train_df.csv', index = False)
     test_df.to_csv(filepath + '/test_df.csv', index = False)
